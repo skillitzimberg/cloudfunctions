88bb36ccaebef93a65da7219ea1f1c2552adb96d
Scott Bergler <commandinghands@gmail.com>
2019-10-23 08:57:09 -0700

    install firebase dependency.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,27 @@
 import * as functions from 'firebase-functions';
 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: 'https://cloudfunctions.firebaseio.com'
+});
+
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const helloWorld = functions.https.onRequest(async (request: any, response: any) => {
+  const message = request.query.text;
+  response.send(`Hello from ${message}!`);
+});
+
+// Take the text parameter passed to this HTTP endpoint and insert it into the
+// Realtime Database under the path /messages/:pushId/original
+exports.addMessage = functions.https.onRequest(async (req: any, res: any) => {
+  // Grab the text parameter.
+  const original = req.query.text;
+  // Push the new message into the Realtime Database using the Firebase Admin SDK.
+  const snapshot = await admin.database().ref('/messages').push({original: original});
+  // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+  res.redirect(303, snapshot.ref.toString());
 });
